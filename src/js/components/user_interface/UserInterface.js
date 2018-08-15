d87d275459fb605edf8c7077fea5691a8983c8b4
Igor Pejic <igor.pejic@dr.com>
2018-08-15 15:53:21 +0200

    fix rotation of NavCycleItems

--- src/js/components/user_interface/UserInterface.js
+++ src/js/components/user_interface/UserInterface.js
@@ -31,19 +31,25 @@ export default class UserInterface extends React.PureComponent {
       const macHeight = this.refs.macbook.offsetHeight;
       let scale = scrollY * 0.003 + 0.3;
       const translateMac = scrollY + (innerHeight/2*0.07*scale);
-      const translateNav = innerWidth > 767 ? 250 : 120 + innerWidth * scrollY * 0.0002;
-      const translateNavX = macHeight * 0.52 * scale
-      const maxScale = innerWidth > 600 ? 1.1 : 1.3;
+      const translateNav = (innerWidth > 767 ? 250 : 120) + innerWidth * scrollY * 0.0002;
+      const translateNavX = macHeight * 0.55 * scale
+      const maxScale = innerWidth > 600 ? 1.0 : 1.3;
 
       if (scale < maxScale) {
         requestAnimationFrame(() => {
           this.refs.container.style.transform = `translateY(${translateMac}px)`;
           this.refs.macbook.style.transform = `scale(${scale - 0.09})`;
           this.cycleNavItems.map((item, index) => {
-            if (scale < 0.7) {
-              return item.style.transform = `rotate(${(index - 1) * 60 - 30}) translate(0, ${translateNav - scrollY}px) rotate(${-(index - 1) * 60 + 30}) scale(${1.5 - scale}`;
+            let step = scale * 600
+            step = step > 240 ? step : 240
+            let transformStr = `rotate(${(index - 1) * 60 + step}deg) translate(0, ${translateNav - scrollY}px) rotate(${-((index - 1) * 60 + step)}deg) scale(${1.5 - scale})`;
+            if (scale < 0.65) {
+              item.style.transform = transformStr;
+            }
+            else if (scale < 0.7) {
+              item.style.transform = `rotate(90deg) rotate(-90deg) scale(${1.7 - scale})`;
             } else {
-              return item.style.transform = `rotate(90deg) translate(${translateNavX}px, ${(35-(14 * index)) * (scale-0.1)}vw) rotate(-90deg) scale(${1.7 - scale})`;
+              item.style.transform = `rotate(90deg) translate(${translateNavX}px, ${(35-(14 * index)) * (scale-0.1)}vw) rotate(-90deg) scale(${1.7 - scale})`;
             }
           });
         });
@@ -102,9 +108,9 @@ export default class UserInterface extends React.PureComponent {
 
   render() {
     return (
-      <div ref="section" className='section--interface pos-relative o-hidden w-100 py-4 px-lg-4 py-md-6'>
-        <h1 className="heading t-center mb-0">{ title }</h1>
+      <div ref="section" className='section--interface pos-relative o-hidden w-100'>
         <div ref="container" className="optimisation-cycle pos-relative w-100 z-6">
+          <h1 className="heading t-center mb-0">{ title }</h1>
           <div ref="macbook" className="interface--container">
               <img src='/assets/images/macbook.png' className="interface--macbook img--fluid" alt="macbook" />
               { this.renderScreen() }
